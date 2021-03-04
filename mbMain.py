39121a404beb552a9f61f16b7f62b7e99c4e52df
monax-kun <monawasensei@gmail.com>
2021-03-04 12:14:01 -0600

    added get_all_downstream_units() method, seems to be working for the most part

--- mbMain.py
+++ mbMain.py
@@ -1,15 +1,30 @@
 import units
+import sys
 
 def main():
 	n01 = units.node("node_01")
+	n02 = units.node("node_02")
+	n02a = units.node("node_02a")
+	n03 = units.node("node_03")
 	s01 = units.stream("stream_01")
 	s02 = units.stream("stream_02")
 	s03 = units.stream("stream_03")
+	s03a = units.stream("stream_03a")
+	s04 = units.stream("stream_04")
+	s04a = units.stream("stream_04a")
+	s05 = units.stream("stream_05")
 
-	n01.i = [s01,s02]
-	n01.o = [s03]
+
+	n01.specify_connections([s01,s02],[s03,s03a])
+	n02.specify_connections([s03],[s04])
+	n03.specify_connections([s04,s04a],[s05])
+	n02a.specify_connections([s03a],[s04a])
 
 	units.make_all_connections()
-
 	units.print_all_connections()
+
+	listTest = s01.get_all_downstream_units()
+	for unit in listTest:
+		print(unit)
+
 main()
--- units.py
+++ units.py
@@ -24,6 +24,47 @@ class unit():
 			print("I couldn't find this shit anywhere dawg")
 		del self #kill yourslef :^)
 
+	def get_all_downstream_units(self,downstreamUnitList = ["init"]):
+		allDownstreamUnits = list()
+		allDownstreamUnits = self.get_downstream_units(downstreamUnitList)
+		return allDownstreamUnits
+
+	def get_downstream_units(self,downstreamUnitList):
+		if len(downstreamUnitList) == 0 or downstreamUnitList[0] == "init":
+			#downstreamUnitList[0] = self
+			downstreamUnitList = []
+		else:
+			pass
+
+		index = len(downstreamUnitList)
+
+		if self.typeOfUnit == "node":
+			if len(self.o) != 0:
+				for stream in self.o:
+					if stream not in downstreamUnitList:
+						downstreamUnitList.append(stream)
+					else:
+						continue
+				for subunit in downstreamUnitList[index:]:
+					if subunit.typeOfUnit == "stream":
+						subunit.get_downstream_units(downstreamUnitList)
+					else:
+						continue
+			else:
+				return []
+		elif self.typeOfUnit == "stream":
+			if self.t != None:
+				if self.t not in downstreamUnitList:
+					downstreamUnitList.append(self.t)
+				self.t.get_downstream_units(downstreamUnitList)
+			else:
+				pass
+		else:
+			print("this shouldn't be happening")
+
+		return downstreamUnitList
+
+
 	def __str__(self):
 		return self.name
 ################################################################################################################################################################
@@ -33,7 +74,14 @@ class node(unit): #right now these just have mixer functionality it looks like
 		unit.__init__(self,name)
 		self.i = list()
 		self.o = list()
+		self.typeOfUnit = "node"
 		unitRegistry["node"].append(self)
+
+	def specify_connections(self,i=list(),o=list()):
+		for stream in i:
+			self.i.append(stream)
+		for stream in o:
+			self.o.append(stream)
 
 	def make_connections(self):
 		for stream in unitRegistry["stream"]:
@@ -72,6 +120,7 @@ class stream(unit):
 		self.tempUnits = tempUnits
 		self.pressure = pressure
 		self.pressUnits = pressUnits
+		self.typeOfUnit = "stream"
 		unitRegistry["stream"].append(self)
 
 	def make_connections(self):
@@ -107,9 +156,22 @@ def make_all_connections():
 		node.make_connections()
 	for stream in unitRegistry["stream"]:
 		stream.make_connections()
-def print_all_connections():
 
+def print_all_connections():
 	for node in unitRegistry["node"]:
 		node.print_connections()
 	for stream in unitRegistry["stream"]:
 		stream.print_connections()
+
+def detect_unconnected_units():
+	unitList = list()
+	for node in unitRegistry["node"]:
+		if len(node.i) == 0 or len(node.o) == 0:
+			unitList.append(node)
+	for stream in unitRegistry["stream"]:
+		if stream.t == None and stream.f == None:
+			unitList.append(stream)
+	if len(unitList) == 0:
+		return 0,unitList
+	else:
+		return 1,unitList
